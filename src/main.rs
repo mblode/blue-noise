568d18f5abcdd6f4cdfc3ba1d4b73bdf1870e4a0
Matthew Blode <m@blode.co>
2021-03-06 13:31:39 +1100

    Push

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::{App, load_yaml};
-use image::{ImageBuffer, Luma, RgbImage};
+use image::{Luma, RgbImage, Rgb};
 
 fn is_bright(noise_color: &Luma<u8>, picture_color: &Luma<u8>) -> bool {
     let noise_luma = noise_color.0;
@@ -32,22 +32,24 @@ fn main() {
     let noise_img = noise_img.as_mut_luma8().unwrap();
     let (noise_width, noise_height) = noise_img.dimensions();
 
-    let _new_img: RgbImage = ImageBuffer::new(old_width, old_height);
+    let mut new_img = RgbImage::new(old_width, old_height);
 
-    let _new_img = ImageBuffer::from_fn(old_width, old_height, |x, y| {
-        let wrap_x = wrap(noise_width, x);
-        let wrap_y = wrap(noise_height, y);
+    for x in 0..old_width {
+        for y in 0..old_height {
+            let wrap_x = wrap(noise_width, x);
+            let wrap_y = wrap(noise_height, y);
 
-        let noise_pixel = noise_img.get_pixel_mut(wrap_x, wrap_y);
-        let old_pixel = old_img.get_pixel_mut(x, y);
+            let noise_pixel = noise_img.get_pixel_mut(wrap_x, wrap_y);
+            let old_pixel = old_img.get_pixel_mut(x, y);
 
-        if is_bright(noise_pixel, old_pixel) {
-            Luma([255u8])
-        } else {
-            Luma([0u8])
+            if is_bright(noise_pixel, old_pixel) {
+                new_img.put_pixel(x, y, Rgb([255, 255, 255]));
+            } else {
+                new_img.put_pixel(x, y, Rgb([0, 0, 0]));
+            }
         }
-    });
+    }
 
-    _new_img.save(&output_file).unwrap();
+    new_img.save(&output_file).unwrap();
     println!("File saved to {}", &output_file);
 }
